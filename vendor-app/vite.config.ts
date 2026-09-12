7ddb9869750ddd916bf4b1697f7cf7e26a8e76a0
mdibrahim31 <mdibrahim.n.2018@gmail.com>
2026-09-13 03:36:46 +0600

    setup github pages

--- vendor-app/vite.config.ts
+++ vendor-app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/dream.project/vendor/',
   plugins: [react(), tailwindcss()],
 });
